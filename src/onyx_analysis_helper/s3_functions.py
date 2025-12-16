11835a195070aada761de9f19a1db78ae2124f60
Deborah Greer <63928724+DebGreer@users.noreply.github.com>
2025-12-16 15:30:06 +0000

    Feat/s3 uploads (#4)
    * feat: Add module containing functions for interacting with s3
    
    - Functions include error handling for calls to s3, adding objects to
    s3, checking that sha256 sums for local and s3 objects match.
    
    * test: Add tests for s3 functions
    
    * build: Add dependencies for s3 functions and s3 testing
    
    * build: Update pyproject.toml
    
    Bump version number
    
    * chore: Lint s3_functions file
    
    * chore: Lint test_s3_functions file
    
    * build: Update test-build.yml
    
    Add boto3 and moto installations to test-build.yml workflow
    
    * feat: Adding function to download object from s3
    
    * test: Add tests and fixtures for download_file_from_s3 function
    
    * test: Update get_s3_checksum tests to use s3_file fixture
    
    * chore: Add pre-commit linting changes
    
    * feat: Add file not found error handling to call to s3 decorator
    
    * feat: Update s3 key structure and simplify download file paths
    
    - s3 key now in format {analysis_id}/{analysis_id}_{climb_id}
    - Download file path removes the {analysis_id}/ prefix from s3 key name
      so file is downloaded to folder specified and not a subfolder
    
    * test: Update tests and fixtures for new s3 key format
    
    * test: Update download file from s3 tests
    
    - Add caplog to tests to check correct error handling
    - Add additional test for handling if outdir doesn't exist
    
    * feat: Update get_s3_sha256sum to retrieve etag
    - Renames function to get_s3_checksum
    - Additional variable to specify checksum to return
    
    * feat: Update generate_local_sha256 to also generate md5
    - Renames to generate_local_checksum
    - Add variable to specify checksum type
    
    * chore: Update name of check_256sums_match to check_checksums_match
    - Reflects changes to other checksum functions to handle md5 and etag
    
    * test: Update and add tests based on new checksum options
    - Updated to new function names
    - Added md5 and etag related fixtures and tests
    - Updated the test json file so not a completely blank file
    
    * build: Update test-build.yml
    
    Update pytest command to give coverage report correctly
    
    * build: Update test-build.yml
    
    Update pytest command to display coverage results in workflow output

---
+++ src/onyx_analysis_helper/s3_functions.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+
+"""
+Module containing functions to assist with the reading of analysis
+objects to and from s3.
+"""
+
+import hashlib
+import logging
+import os
+from functools import wraps
+from pathlib import Path
+
+import boto3
+import regex as re
+from botocore.config import Config
+from botocore.exceptions import ClientError
+
+
+def call_to_s3(func):
+    """Decorator that provides error handling for any calls to s3.
+    If call is successful, returns the result and an exitcode of 0.
+    If call is unsucessful, returns None and an exitcode of 1.
+    """
+
+    @wraps(func)
+    def call_to_s3_wrapper(*args, **kwargs):
+        try:
+            logging.debug("Attempting connection to s3")
+            result, exitcode = func(*args, **kwargs)
+            logging.debug("Successful connection to s3")
+
+            return result, exitcode
+
+        except ClientError as exc:
+            logging.error("Client error: %s.", exc)
+            result = None
+            exitcode = 1
+
+            return result, exitcode
+
+        except FileNotFoundError as exc:
+            logging.error("Check file or directory path is correct: %s", exc)
+            result = None
+            exitcode = 1
+
+            return result, exitcode
+
+        except Exception as exc:
+            logging.error("Unhandled error: %s", exc)
+            result = None
+            exitcode = 1
+
+            return result, exitcode
+
+    return call_to_s3_wrapper
+
+
+def set_up_s3_client(endpoint_url: str = "https://s3.climb.ac.uk") -> boto3.client:
+    """Sets up s3 client including config options such as number of retries.
+    Default for endpoint_url can be overridden for testing purposes.
+    Returns an s3 client.
+    """
+
+    s3_config = Config(retries={"total_max_attempts": 3, "mode": "standard"})
+
+    s3_client = boto3.client("s3", config=s3_config, endpoint_url=endpoint_url)
+
+    return s3_client
+
+
+@call_to_s3
+def upload_file_to_s3(
+    analysis_id: str, bucket: str, file_for_upload: os.path, s3_client: boto3.client
+) -> str:
+    """Uploads a file to s3 bucket using the analysis ID to generate a path
+    Arguments:
+        analysis_id -- Name of analysis in onyx
+        bucket -- Name of bucket in s3
+        file_for_upload -- Analysis file to be uploaded to s3
+        s3_client -- Client for interacting with s3
+    Returns tuple of:
+        s3_uri - Path to object in s3
+        exitcode - Exit status
+    """
+    # Make name for s3 object that includes analysis ID
+    s3_key = _make_s3_key_name(analysis_id, file_for_upload)
+
+    # Upload file
+    s3_client.upload_file(file_for_upload, bucket, s3_key)
+    s3_uri = f"s3://{bucket}/{s3_key}"
+    exitcode = 0
+
+    return s3_uri, exitcode
+
+
+def _make_s3_key_name(analysis_id: str, file_for_upload: os.path) -> str:
+    """Create a key name for the file to be uploaded to s3 that includes
+    the analysis ID and analysis type.
+    Arguments:
+        analysis_id -- Name of analysis in onyx
+        file_for_upload -- Analysis file to be uploaded to s3
+    Returns:
+        s3_key - Name for object in s3
+    """
+    # Get name of file without rest of path
+    upload_file = Path(file_for_upload).name
+
+    # Join with analysis id
+    s3_key = f"{analysis_id}/{analysis_id}_{upload_file}"
+
+    return s3_key
+
+
+def generate_local_checksum(file_for_upload: os.path, checksum_type: str) -> str:
+    """Generates a checksum for local copy of file that will be
+    uploaded. Returns sha256 or md5 checksum"""
+    with Path(file_for_upload).open("rb") as file:
+        digest = hashlib.file_digest(file, f"{checksum_type}")
+
+    checksum = digest.hexdigest()
+
+    return checksum
+
+
+@call_to_s3
+def get_s3_checksum(bucket: str, s3_key: str, s3_client: boto3.client, checksum_type: str):
+    """Retrieves checksum from s3 object metadata.
+    Arguments:
+        bucket -- Name of bucket in s3 where object is stored
+        s3_key -- Name of object in s3
+        s3_client -- Client for interacting with s3
+        checksum_type -- Algorithm for checksum
+    Returns tuple of:
+        checksum -- sha256 or etag sum for s3 object, None if error
+        exitcode - 0 for success, 1 for failure
+    """
+    response = s3_client.head_object(Bucket=bucket, Key=s3_key)
+
+    if checksum_type == "sha256":
+        checksum = response["ResponseMetadata"]["HTTPHeaders"]["x-amz-content-sha256"]
+
+    elif checksum_type == "etag":
+        checksum = response["ResponseMetadata"]["HTTPHeaders"]["etag"]
+        # Get md5 from within nested "" in etag
+        checksum = re.search('"(.*)"', checksum).group(1)
+    else:
+        logging.error("Invalid checksum type provided, provide one of sha256 or etag")
+        result = None
+        exitcode = 1
+
+        return result, exitcode
+
+    exitcode = 0
+
+    return checksum, exitcode
+
+
+def check_checksums_match(local_checksum, s3_checksum):
+    """Check s3 checksum and local checksum match. Returns
+    exitcode of 0 if checksums match, 1 if they do not.
+    """
+
+    if local_checksum == s3_checksum:
+        logging.info("Local and s3 checksums match")
+        return 0
+    else:
+        logging.error("Local and s3 checksums do not match")
+        return 1
+
+
+@call_to_s3
+def download_file_from_s3(s3_client: boto3.client, bucket: str, s3_key: str, out_dir: os.path):
+    """Downloads object from s3.
+     Arguments:
+        s3_client -- Client for interacting with s3
+        bucket -- Name of bucket in s3 where object is stored
+        s3_key -- Name of object in s3
+        out_dir -- Location to download the file to
+    Returns tuple of:
+        result_file -- Path to downloaded s3 object
+        exitcode - 0 for success, 1 for failure
+    """
+    s3_key_no_prefix = s3_key.split("/")[-1]
+    result_file = Path(out_dir) / f"{s3_key_no_prefix}"
+
+    s3_client.download_file(bucket, s3_key, result_file)
+
+    exitcode = 0
+
+    return result_file, exitcode
---
+++ tests/test_s3_functions.py
@@ -0,0 +1,228 @@
+"""
+Unit tests for s3 functions included in this repo. To save
+generated test results in the repo, run the following command from the
+top folder:
+pytest tests/test_s3_functions.py -rP --basetemp tests/test_outputs/
+
+WARNING: Using --basetemp on an existing folder will overwrite all files.
+"""
+
+import os
+
+import boto3
+import pytest
+from moto import mock_aws
+from moto.server import ThreadedMotoServer
+
+from onyx_analysis_helper import s3_functions as s3f
+
+
+# Fixtures
+@pytest.fixture(scope="module")
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+
+
+@pytest.fixture(scope="module")
+def moto_server(aws_credentials):
+    """Fixture to run a mocked AWS server for testing."""
+
+    # Note: pass `port=0` to get a random free port.
+    server = ThreadedMotoServer(port=0)
+    server.start()
+    host, port = server.get_host_and_port()
+    yield f"http://{host}:{port}"
+    server.stop()
+
+
+@pytest.fixture
+def s3_client(moto_server):
+    with mock_aws():
+        s3_client = boto3.client("s3", endpoint_url=moto_server)
+        yield s3_client
+
+
+@pytest.fixture
+def test_bucket(s3_client):
+    s3_client.create_bucket(Bucket="testbucket")
+
+
+@pytest.fixture
+def example_result_file():
+    result_dir = "tests/test_data/C-123456789_qc_results.json"
+
+    return result_dir
+
+
+@pytest.fixture
+def example_result_file_sha256():
+    checksum = "ce468c79b716ec9dba66f3cbe88e7182e41692a1120f1040ac7958f077470bac"
+
+    return checksum
+
+
+@pytest.fixture
+def example_result_file_md5():
+    checksum = "64b73b75b60fc4ebf043077bc887bb46"
+
+    return checksum
+
+
+@pytest.fixture
+def s3_file(s3_client, test_bucket, example_result_file):
+    s3_client.upload_file(
+        example_result_file, "testbucket", "A-1234/A-1234_C-123456789_qc_results.json"
+    )
+
+
+@pytest.fixture
+def download_file_path(tmp_path_factory):
+    tmp_dir = tmp_path_factory.mktemp("test_outputs")
+    return str(tmp_dir)
+
+
+# Tests
+def test_make_s3_name(example_result_file):
+    s3_key = s3f._make_s3_key_name(analysis_id="A-1234", file_for_upload=example_result_file)
+    print(s3_key)
+    assert s3_key == "A-1234/A-1234_C-123456789_qc_results.json"
+
+
+@mock_aws
+def test_set_up_s3_client(moto_server):
+    s3_client = s3f.set_up_s3_client(endpoint_url=moto_server)
+
+    s3_client.create_bucket(Bucket="test")
+    result = s3_client.list_buckets()
+
+    assert len(result["Buckets"]) == 1
+
+
+@mock_aws
+def test_upload_file_to_s3(s3_client, test_bucket, example_result_file, example_result_file_sha256):
+    tuple_return = s3f.upload_file_to_s3(
+        analysis_id="A-1234",
+        bucket="testbucket",
+        file_for_upload=example_result_file,
+        s3_client=s3_client,
+    )
+
+    response = s3_client.head_object(
+        Bucket="testbucket", Key="A-1234/A-1234_C-123456789_qc_results.json"
+    )
+
+    assert tuple_return == ("s3://testbucket/A-1234/A-1234_C-123456789_qc_results.json", 0)
+    assert (
+        response["ResponseMetadata"]["HTTPHeaders"]["x-amz-content-sha256"]
+        == example_result_file_sha256
+    )
+
+
+@mock_aws
+def test_upload_file_to_s3_client_error_handling(s3_client, test_bucket, example_result_file):
+    tuple_return = s3f.upload_file_to_s3(
+        analysis_id="A-1234",
+        bucket="wrongbucket",
+        file_for_upload=example_result_file,
+        s3_client=s3_client,
+    )
+
+    assert tuple_return == (None, 1)
+
+
+@mock_aws
+def test_upload_file_to_s3_generic_error_handling(s3_client, test_bucket):
+    tuple_return = s3f.upload_file_to_s3(
+        analysis_id="A-1234",
+        bucket="testbucket",
+        file_for_upload="notafile.json",
+        s3_client=s3_client,
+    )
+
+    assert tuple_return == (None, 1)
+
+
+def test_generate_local_checksum_sha256(example_result_file, example_result_file_sha256):
+    checksum = s3f.generate_local_checksum(example_result_file, "sha256")
+
+    assert checksum == example_result_file_sha256
+
+
+def test_generate_local_checksum_md5(example_result_file, example_result_file_md5):
+    checksum = s3f.generate_local_checksum(example_result_file, "md5")
+
+    assert checksum == example_result_file_md5
+
+
+@mock_aws
+def test_get_s3_checksum_sha256(s3_client, test_bucket, example_result_file_sha256, s3_file):
+    tuple_return = s3f.get_s3_checksum(
+        "testbucket", "A-1234/A-1234_C-123456789_qc_results.json", s3_client, "sha256"
+    )
+
+    assert tuple_return == (example_result_file_sha256, 0)
+
+
+@mock_aws
+def test_get_s3_checksum_etag(s3_client, test_bucket, example_result_file_md5, s3_file):
+    tuple_return = s3f.get_s3_checksum(
+        "testbucket", "A-1234/A-1234_C-123456789_qc_results.json", s3_client, "etag"
+    )
+
+    assert tuple_return == (f"{example_result_file_md5}", 0)
+
+
+@mock_aws
+def test_get_s3_checksum_error_handling(s3_client, test_bucket, s3_file):
+    tuple_return = s3f.get_s3_checksum("testbucket", "wrongkey", s3_client)
+
+    assert tuple_return == (None, 1)
+
+
+def test_check_checksums_match_pass(example_result_file_sha256):
+    result = s3f.check_checksums_match(example_result_file_sha256, example_result_file_sha256)
+
+    assert result == 0
+
+
+def test_check_sha256sums_match_fail(example_result_file_sha256):
+    result = s3f.check_checksums_match(example_result_file_sha256, "incorrectsha256string")
+
+    assert result == 1
+
+
+@mock_aws
+def test_download_file_from_s3(s3_client, test_bucket, s3_file, download_file_path):
+    out_file, exitcode = s3f.download_file_from_s3(
+        s3_client, "testbucket", "A-1234/A-1234_C-123456789_qc_results.json", download_file_path
+    )
+
+    assert out_file.exists()
+    assert exitcode == 0
+
+
+@mock_aws
+def test_download_file_from_s3_error_handling(
+    s3_client, test_bucket, s3_file, download_file_path, caplog
+):
+    tuple_return = s3f.download_file_from_s3(
+        s3_client, "testbucket", "wrongkey", download_file_path
+    )
+
+    assert tuple_return == (None, 1)
+    assert "Client error" in caplog.text
+
+
+@mock_aws
+def test_download_file_from_s3_error_no_outdir_exists(s3_client, test_bucket, s3_file, caplog):
+    tuple_return = s3f.download_file_from_s3(
+        s3_client, "testbucket", "A-1234/A-1234_C-123456789_qc_results.json", "notadir"
+    )
+
+    assert tuple_return == (None, 1)
+    assert "Check file or directory" in caplog.text
