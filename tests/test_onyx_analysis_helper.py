a53d88d6fbc24497e8cbf2b71db63858041cdf6f
Jess Friedersdorff <30495397+JessF298@users.noreply.github.com>
2026-09-23 09:38:46 +0100

    tests: rename unittest.

--- tests/test_onyx_analysis_helper.py
+++ tests/test_onyx_analysis_helper.py
@@ -675,7 +675,7 @@ def test_check_analysis_attributes_fail(invalid_field_dict, caplog):
     assert attr_fail
 
 
-def test_check_analysis_attributes_new_scape(complete_field_dict):
+def test_check_analysis_attributes_new_scape_fail(complete_field_dict):
     field_dict = complete_field_dict.copy()
     field_dict["newscape_records"] = []
     analysis = oa.OnyxAnalysis()
